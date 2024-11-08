cf3483453a5edf43df2b7ea5597ba9484020ffec
jokole3 <paesdoorball@mail.ru>
2024-11-08 19:58:55 +0500

    message

--- main.c
+++ main.c
@@ -4,8 +4,6 @@ int main(int argc, char **argv)
 { 
     char first[255], last[255]; 
     askname(first); 
-    askname(last);
-    askname(lastlast);
     printf("Hello, %s %s!\n", first, last); 
     return 0; 
 } 
